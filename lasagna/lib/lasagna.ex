4df44b850e53ad46b03f5efcf299ca10661bbe6f
Karol-Soares <karolina.soares@sumup.com>
2022-05-11 19:02:59 -0300

    Implement a different solution

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,12 +1,15 @@
 defmodule Lasagna do
-  def expected_minutes_in_oven, do: 40
 
-  def remaining_minutes_in_oven(num), do: num - 10 
+  @minutes_in_total 40
+  @minutes_per_layer 2
 
-  def preparation_time_in_minutes(layers), do: layers + layers
+  def expected_minutes_in_oven(), do: @minutes_in_total
 
-  def total_time_in_minutes(layers, minutes), do: (layers + layers) + minutes
+  def remaining_minutes_in_oven(passed), do: expected_minutes_in_oven - passed
 
-  def alarm, do: "Ding!"
+  def preparation_time_in_minutes(layers), do: layers * @minutes_per_layer
+
+  def total_time_in_minutes(layers, passed), do: preparation_time_in_minutes(layers) + passed
+
+  def alarm(), do: "Ding!"
 end
-
